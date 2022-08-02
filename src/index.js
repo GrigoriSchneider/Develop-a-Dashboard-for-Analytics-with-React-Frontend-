6bbec3fb16e4c554721c29e824aa9de8af376d9a
GrigoriSchneider <grigorischneider@gmail.com>
2022-08-02 12:13:08 +0200

    removed some comments

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client';
 import Home from './page/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HorizontalNavigation from './components/HorizontalNavigation';
-// import Navbar from './components/Navbar';
-// import Home from './page/Home';
-// import About from './page/About';
-// import Error from './page/Error';
-// import { RoomProvider } from './context';
-// import Appartment from './page/Appartment';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
